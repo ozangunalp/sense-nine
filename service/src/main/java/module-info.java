c3e23513982447f282012bc6bdfd26ff9fe0d5c1
ogu <Ozan.Gunalp@lesfurets.com>
2017-10-27 13:29:48 +0200

    Fix maven compile error

--- service/src/main/java/module-info.java
+++ service/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.mechanitis.demo.sense.service {
     requires jetty.server;
     requires jetty.servlet;
     requires javax.websocket.server.impl;
+    requires javax.websocket.client.impl;
 
     exports com.mechanitis.demo.sense.service;
 }
